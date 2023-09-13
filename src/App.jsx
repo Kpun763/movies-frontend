7ace2360e7e11e3a2130588cf3ae72bd3e61c006
Kyle Washington <kmw1993@msn.com>
2023-09-13 15:46:26 -0400

    added the fetchMovie method

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,48 @@
-import './App.css';
-import Header from './components/Header/Header'
-import MovieList from './components/MovieList/MovieList'
-import MovieInfo from './components/MovieInfo/MovieInfo';
-import NewMovieForm from './components/NewMovieForm/NewMovieForm';
-import React, { useState, useEffect } from 'react';
-import initData from './data/data';
+import "./App.css";
+import Header from "./components/Header/Header";
+import MovieList from "./components/MovieList/MovieList";
+import MovieInfo from "./components/MovieInfo/MovieInfo";
+import NewMovieForm from "./components/NewMovieForm/NewMovieForm";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [activeIndex, setActiveIndex] = useState(-1);
-  
 
-  useEffect(() => {
-    setMovies(initData)
-  }, []);
-
-
-  const handleNewMovie = (newMovie) => {
-      const updatedMovies = [...movies, newMovie];
-      setMovies(updatedMovies)
+  const fetchMovies = async () => {
+    try {
+      const response = await axios.get("https://localhost:7251/api/Movies");
+      // console.log(response);
+      setMovies (response.data)
+    } catch (error) {
+      console.warn("Error in fetchMovies request: ", error);
+    }
   };
 
-  const selectedMovie = movies[activeIndex]
+  useEffect(() => {
+    fetchMovies()
+  }, []);
 
+  const handleNewMovie = (newMovie) => {
+    const updatedMovies = [...movies, newMovie];
+    setMovies(updatedMovies);
+  };
+
+  const selectedMovie = movies[activeIndex];
 
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      
       <div className="flex-container">
-        <MovieList movies={movies} activeIndex={activeIndex} setActiveIndex={setActiveIndex}/>
+        <MovieList
+          movies={movies}
+          activeIndex={activeIndex}
+          setActiveIndex={setActiveIndex}
+        />
         <MovieInfo movieObj={selectedMovie} />
-        <NewMovieForm onNewMovie={handleNewMovie}/>
+        <NewMovieForm onNewMovie={handleNewMovie} />
       </div>
     </div>
   );
