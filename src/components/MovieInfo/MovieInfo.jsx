ac9029cad32368ad4643ed8e62d98680e14c8a0a
Kyle Washington <kmw1993@msn.com>
2023-09-14 14:23:26 -0400

    added some conditional rendering in movieinfo

--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -1,49 +1,53 @@
-import './MovieInfo.css';
-import React, { useState, useEffect } from 'react';
+import "./MovieInfo.css";
+import React, { useState, useEffect } from "react";
 
-import axios from 'axios';
+import axios from "axios";
 
 const MovieInfo = ({ movieObj }) => {
-    const [flixData, setFlixData] = useState(null);
+  const [flixData, setFlixData] = useState(null);
 
-    console.log('flixData:', flixData);
+  console.log("flixData:", flixData);
 
-    const fetchFlixData = async () => {
-        try{
-            const response = await axios.get(`https://flixdb.glitch.me/api/movies?key=3Hd9zT4wQ6LmB7pY2jK1Xr8V&title=${movieObj.title}`,
-            );
-            setFlixData(response.data)
-        }
-        catch(error) {
-            console.warn("Error fetching flix data in MovieInfo: ", error)
-        }
-    };
+  const fetchFlixData = async () => {
+    try {
+      const response = await axios.get(
+        `https://flixdb.glitch.me/api/movies?key=3Hd9zT4wQ6LmB7pY2jK1Xr8V&title=${movieObj.title}`
+      );
+      setFlixData(response.data);
+    } catch (error) {
+      console.warn("Error fetching flix data in MovieInfo: ", error);
+      setFlixData(null)
+    }
+  };
 
-    useEffect(() => {
-        fetchFlixData();
-    }, [movieObj]);
+  useEffect(() => {
+    movieObj && fetchFlixData();
+  }, [movieObj]);
 
-    return movieObj && ( 
-    <div className="flex-item">
+  return (
+    movieObj && (
+      <div className="flex-item">
         <h4>{movieObj.title}</h4>
-        <div className="poster-wrapper">
-            <img src={flixData?.poster_url}/>
-        </div>
+       {flixData && <div className="poster-wrapper">
+          <img src={flixData?.poster_url} />
+        </div>}
         <div>
-            <div className="data-item">
-                <span>Running Time</span>
-                <span>{movieObj.runningTime}</span>
-            </div>
-            <div className="data-item">
-                <span>Genre</span>
-                <span>{movieObj.genre}</span>
-            </div>
-            <div className="data-item">
-                <span>Release Data</span>
-                <span>{flixData?.release_data}</span>
-            </div>
+          <div className="data-item">
+            <span>Running Time</span>
+            <span>{movieObj.runningTime}</span>
+          </div>
+          <div className="data-item">
+            <span>Genre</span>
+            <span>{movieObj.genre}</span>
+          </div>
+        {flixData && <div className="data-item">
+            <span>Release Data</span>
+            <span>{flixData?.release_data}</span>
+          </div>}
         </div>
-    </div> );
+      </div>
+    )
+  );
 };
- 
+
 export default MovieInfo;
